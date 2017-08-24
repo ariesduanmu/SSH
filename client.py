94155ed90872047da7861ef946b67063ac413b9e
Qin <aries.duanmu@gmail.com>
2017-08-24 19:32:59 +0800

    Authentication failed in rsa, password works

--- client.py
+++ client.py
@@ -1,13 +1,143 @@
 import paramiko
 import sys
+import getopt
+import os
+import socket
 
-host = sys.argv[1]
-port = int(sys.argv[2])
+paramiko.util.log_to_file('client.log')
 
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
-client.connect(host,port,'robit','human')
-ssh_session = client.get_transport().open_session()
-if ssh_session.active:
-    ssh_session.send('ClientConnected')
-    print ssh_session.recv(1024)
+key_filename = ''
+username = password = ""
+host = ""
+port = 22
+
+
+
+def ssh_client():
+	
+	try:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect((host,port))
+	except Exception as e:
+		print('[-] Connect failed: ' + str(e))
+		sys.exit(1)
+	try:
+		t = paramiko.Transport(sock)
+		try:
+			t.start_client()
+		except paramiko.SSHException:
+			print "[-] SSH negotiation failed"
+			sys.exit(1)
+
+		try:
+			keys = paramiko.util.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
+		except IOError:
+			try:
+				keys = paramiko.util.load_host_keys(os.path.expanduser('~/ssh/known_hosts'))
+			except IOError:
+				print '[-] Unable to open host keys file'
+				keys = {}
+
+		key = t.get_remote_server_key()
+		if host not in keys:
+			print '[*] WARNING: Unknown host key!'
+		elif key.get_name() not in keys[host]:
+			print '[*] WARNING: Unknown host key!'
+		elif keys[host][key.get_name()] != key:
+			print '[*] WARNING: Host key has changed!!!'
+			sys.exit(1)
+		else:
+			print('[+] Host key OK.')
+
+		if len(password) > 0:
+			t.auth_password(username,password)
+		elif len(key_filename) > 0:
+			try:
+				key = paramiko.RSAKey.from_private_key_file(key_filename)
+			except paramiko.PasswordRequiredException:
+				magic = getpass.getpass('RSA key password: ')
+				key = paramiko.RSAKey.from_private_key_file(path, magic)
+			t.auth_publickey(username, key)
+		if not t.is_authenticated():
+			print('[-] Authentication failed. :(')
+			t.close()
+			sys.exit(1)
+
+		chan = t.open_session()
+		chan.get_pty()
+		chan.invoke_shell()
+		print '[+] Here we go!\n'
+		interactive.interactive_shell(chan)
+		chan.close()
+		t.close()
+
+	except Exception as e:
+	    print '[+] Caught exception: ' + str(e.__class__) + ': ' + str(e)
+	    
+	    try:
+	        t.close()
+	    except:
+	        pass
+	    sys.exit(1)
+	'''
+
+	
+	try:
+		client = paramiko.SSHClient()
+		client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		if len(key_filename) == 0:
+			client.connect(host,port,username,password)
+		else:
+			key = paramiko.RSAKey.from_private_key_file(key_filename)
+			client.connect(host,port,username,pkey = key_filename)
+		ssh_session = client.get_transport().open_session()
+		if ssh_session.active:
+		    ssh_session.send('ClientConnected')
+		    print ssh_session.recv(1024)
+
+	except Exception as e:
+		print str(e)
+		sys.exit(1)
+	'''
+	
+def usage():
+    print "SSH Paramiko based SSH tool @Ludisposed & @Qin"
+    print ""
+    print "Usage: client.py username@host"
+    print ""
+    print "Optionals"
+    print "-p --port                 - port use to connect, default is 22"
+    print "-i                        - server pub key's filename"
+    print ""
+    print "Examples: "
+    print "python2.7 client.py username@host"
+    print "python2.7 client.py username@host -p port"
+    print "python2.7 client.py username@host -p port -i filename"
+
+if __name__ == "__main__":
+    if not len(sys.argv[1]):
+        usage()
+    else:
+        try:
+            username, host = sys.argv[1].split('@')
+        except Exception as e:
+            print str(e)
+            usage()
+    if len(sys.argv[2:]) > 0:
+        try:
+            ops, args = getopt.getopt(sys.argv[2:], "p:i:", ["port="])
+        except getopt.GetoptError as err:
+            print str(err)
+            usage()
+
+        for o,a in ops:
+            if o in ('-p','--port'):
+                port = int(a)
+            elif o in ('-i'):
+            	filename = a
+    
+    if len(filename) == 0:
+    	password = raw_input('Password: ')
+    
+    ssh_client() 
+    
--- server.py
+++ server.py
@@ -3,58 +3,111 @@ from binascii import hexlify
 import os
 import socket
 import sys
+import getopt
 import threading
 import traceback
 
 import paramiko
 from paramiko.py3compat import b, u, decodebytes
 
+paramiko.util.log_to_file('server.log')
 
 host_key = paramiko.RSAKey(filename='test_rsa.key')
+public_key_data = (b'AAAAB3NzaC1yc2EAAAABIwAAAIEAyO4it3fHlmGZWJaGrfeHOVY7RWO3P9M7hp'
+                   b'fAu7jJ2d7eothvfeuoRFtJwhUmZDluRdFyhFY/hFAh76PJKGAusIqIQKlkJxMC'
+                   b'KDqIexkgHAfID/6mqvmnSJf0b5W8v5h2pI/stOSwTQ+pxVhwJ9ctYDhRSlF0iT'
+                   b'UWT10hcuO4Ks8=')
+public_key = paramiko.RSAKey(data = decodebytes(public_key_data))
+username = password = ""
+port = 22
+
 
 class Server (paramiko.ServerInterface):
-
+    def __init__(self):
+        self.event = threading.Event()
     def check_channel_request(self, kind, channelid):
     	if kind == "session":
     		return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
     def check_auth_password(self, u, p):
-        if(u =='robit' and p=='human'):
+        if(u == username and p == password):
             return paramiko.AUTH_SUCCESSFUL
         return paramike.AUTH_FAILED
+    def check_auth_publickey(self, username, key):
+        if(username == username) and (key == public_key):
+            return paramiko.AUTH_SUCCESSFUL
+        return paramiko.AUTH_FAILED
 
-host = sys.argv[1]
-port = int(sys.argv[2])
 
-try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((host, port))
-    sock.listen(5)
-    print "[+] Listening for connection..."
-    client, addr = sock.accept()
-except Exception,e:
-    print "[-] Listen failed: " + str(e)
-    sys.exit(1)
-print "[+] Got a connection"
-
-try:
-    bhSession = paramiko.Transport(client)
-    bhSession.add_server_key(host_key)
-    server = Server()
+def ssh_server():
     try:
-        bhSession.start_server(server = server)
-    except paramiko.SSHException, x:
-        print "[-] SSH negotiation failed"
-    chan = bhSession.accept(20)
-    print "[+] Authenticated"
-    print chan.recv(1024)
-    chan.send('Welcome to the SSH')
-except Exception, e:
-    print "[-] Caught exception: " + str(e)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("localhost", port))
+        sock.listen(5)
+        print "[+] Listening for connection..."
+        client, addr = sock.accept()
+    except Exception,e:
+        print "[-] Listen failed: " + str(e)
+        sys.exit(1)
+    print "[+] Got a connection"
+
     try:
-        bhSession.close()
-    except:
-        pass
-    sys.exit(1)
+        bhSession = paramiko.Transport(client)
+        bhSession.add_server_key(host_key)
+        server = Server()
+        try:
+            bhSession.start_server(server = server)
+        except paramiko.SSHException, x:
+            print "[-] SSH negotiation failed"
+        chan = bhSession.accept(20)
+        print "[+] Authenticated"
+        print chan.recv(1024)
+        chan.send('Welcome to the SSH')
+    except Exception, e:
+        print "[-] Caught exception: " + str(e)
+        try:
+            bhSession.close()
+        except:
+            pass
+        sys.exit(1)
+
+def usage():
+    print "SSH Paramiko based SSH tool @Ludisposed & @Qin"
+    print ""
+    print "Usage: server.py -u username -m password"
+    print "-u --username                 - username use to auth"
+    print "-m --magic                    - password use to auth"
+    print ""
+    print "Optionals"
+    print "-p --port                     - port use for ssh server, default is 22"
+    print ""
+    print "Examples: "
+    print "python2.7 server.py -u username -m password"
+    print "python2.7 server.py -p port -u username -m password"
+
+if __name__ == "__main__":
+    if not len(sys.argv[1:]):
+        usage()
+    else:
+        try:
+            ops, args = getopt.getopt(sys.argv[1:], "p:u:m:", ["port=","username=","magic="])
+        except getopt.GetoptError as err:
+            print str(err)
+            usage()
+
+        for o,a in ops:
+            if o in ('-p','--port'):
+                port = int(a)
+            elif o in ('-u','--username'):
+                username = a
+            elif o in ('-m','--magic'):
+                password = a
+
+        if len(username) > 0 and len(password) > 0:
+           ssh_server() 
+        else:
+            usage()
+    
+
 
